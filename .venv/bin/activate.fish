bf37823126031d6889531becd9bdbcb771963795
Ubuntu <ubuntu@ip-172-31-1-2.eu-north-1.compute.internal>
2024-03-18 08:16:28 +0000

    add:unicorn

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/tornado-02/Desktop/AWS-repository/.venv"
+set -gx VIRTUAL_ENV "/home/ubuntu/AWS-repository/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
